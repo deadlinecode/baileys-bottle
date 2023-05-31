61d4241d04dedec8b123516e40eea62c345c6205
Robson Rissato <92188396+robsonwr@users.noreply.github.com>
2023-05-31 15:54:55 -0300

    Update example.ts
    ADD @whiskeysockets/baileys

--- src/example/example.ts
+++ src/example/example.ts
@@ -1,8 +1,8 @@
 import makeWASocket, {
   DisconnectReason,
   fetchLatestBaileysVersion,
-} from "@adiwajshing/baileys";
-import log from "@adiwajshing/baileys/lib/Utils/logger";
+} from "@whiskeysockets/baileys";
+import log from "@whiskeysockets/baileys/lib/Utils/logger";
 import BaileysBottle from "..";
 import { Boom } from "@hapi/boom";
 
